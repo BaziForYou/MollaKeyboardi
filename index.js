c79036464a2c1578cecb1fffaedb70ad40cf8b51
Mohammad amin Jafarian <amin.jafarian82@gmail.com>
2022-12-05 19:57:46 +0330

    language pattern logic

--- index.js
+++ index.js
@@ -15,6 +15,31 @@ console.info("Generating Bot System...");
 const bot = new Telegraf(getENV('BOT_TOKEN'));
 const ListeningCommands = getENV('Listening_Words').toLowerCase().split(',')
 
+const engPattern = "qwertyuiop[]\\asdfghjkl;'zxcvbnm,./";
+const FaPattern = "ضصثقفغعهخحجچپشسيبلاتنمكگظطزرذدئو./";
+
+function convertPattern(text){
+    let result = "";
+    let usedPattern = "";
+    let finalPattern = "";
+    if(/[a-zA-Z]/.test(text)){
+        usedPattern = engPattern;
+        finalPattern = FaPattern;
+    }else if (/[آ-ی]/.test(text)){
+        usedPattern = FaPattern;
+        finalPattern = engPattern;
+    }
+    if (usedPattern.length === 0) return "text pattern is not valid just support english and persian";
+    for(let i = 0; i < text.length; i++){
+        let index = usedPattern.indexOf(text[i]);
+        if(index !== -1){
+            result += finalPattern[index];
+        }else{
+            result += text[i];
+        }
+    }
+    return result;
+}
 
 
 console.info("Starting Bot");
